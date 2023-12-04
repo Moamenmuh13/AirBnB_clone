65d682479ad28a40aee5510fdc6aa6e86ba4f3cc
ConsumedKing <mustafa_165953@fci.sohag.edu.eg>
2023-12-05 01:41:26 +0200

    Solved Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,19 @@ class BaseModel():
         BaseModel Class
         all common attributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = dt.datetime.now()
         self.updated_at = self.created_at
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key in ["created_at", "updated_at"]:
+                    setattr(self, key, dt.datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
         return f"{self.__class__.__name__} ({self.id} {self.__dict__})"
