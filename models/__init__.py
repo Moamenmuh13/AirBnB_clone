f11cc101a28d3a620caaff814561f072e479b5a7
ConsumedKing <mustafa_165953@fci.sohag.edu.eg>
2023-12-11 00:27:15 +0200

    __init__.py docstring

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """
 This package provides utilities for working with airbnb console app
+the airbnb clone porject is clone of the original airbnb but right now
+it only works in terminal
 """
 from .engine import file_storage as fs
 
